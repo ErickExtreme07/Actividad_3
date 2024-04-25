b872a18e547a555ae57aac204be1373924770d07
ErickExtreme07 <ericke057@gmail.com>
2024-04-24 22:59:05 -0500

    Ingreso de un Visitante al PH Marino

---
+++ src/Visitantes.java
@@ -0,0 +1,30 @@
+public class Visitantes {
+
+    public static void main(String[] args) {
+
+        String Nombre= "Erick";
+        Byte Edad = 36;
+        String Cedula = "8-821-121";
+        Short peso = 155;
+        Long NCasa = 355L;
+        char Sexo ='M';
+        Boolean Entrada = true;
+        Float Altura = 1.7f;
+        Double Altura2 = 1.7;
+
+        System.out.println("Bienvenido " + Nombre + " al PH Marino");
+        System.out.println("Sus Datos Registrados son: ");
+        System.out.println("Su Cédula es: " + Cedula);
+        System.out.println("Su Edad: " + Edad);
+        System.out.println("Su Sexo: " + Sexo);
+        System.out.println("Su Peso: " + peso + "lb");
+        System.out.println("Su Altura: " + Altura + "M");
+        System.out.println("Su Altura 2: " + Altura2 + "M");
+        System.out.println("Casa Numero: " + NCasa + "-B");
+        System.out.println("Logro Ingresar " + Entrada);
+
+
+    }
+
+
+}
